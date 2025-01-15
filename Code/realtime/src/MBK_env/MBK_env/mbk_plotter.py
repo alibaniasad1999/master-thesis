2f88902a5f649c45c85debcf440ecda75da110e9
Ali <alibaniasad1999@yahoo.com>
2025-01-15 17:38:40 +0330

    fix gpt

--- Code/realtime/src/MBK_env/MBK_env/mbk_plotter.py
+++ Code/realtime/src/MBK_env/MBK_env/mbk_plotter.py
@@ -8,13 +8,13 @@ from matplotlib.animation import FuncAnimation
 class ArraySubscriber(Node):
     def __init__(self):
         super().__init__('mbk_subscriber')
+        self.ani = None
         self.subscription = self.create_subscription(
             Float64MultiArray,
             'mass_spring_damper_env',
             self.listener_callback,
             10
         )
-        self.subscription  # prevent unused variable warning
         self.data = []  # Store data for plotting
         self.fig, self.ax = plt.subplots()
         self.line, = self.ax.plot([], [], 'r-', lw=2)
@@ -28,24 +28,24 @@ class ArraySubscriber(Node):
         self.get_logger().info(f'Received data: {self.data}')
 
     def animate(self, frame):
-        # Plot the current data (only works for size 2)
-        self.line.set_data([0, 1], self.data)
+        # Update the plot only if there is data
+        if len(self.data) == 2:
+            self.line.set_data([0, 1], self.data)
+        else:
+            self.line.set_data([], [])  # Clear the plot if data is not valid
         return self.line,
 
     def run_plot(self):
         # Create the animation
         self.ani = FuncAnimation(self.fig, self.animate, blit=True)
-        # plt.show()
+        plt.show()
 
 def main(args=None):
     rclpy.init(args=args)
     node = ArraySubscriber()
 
-    # Start the plotting in a separate thread
-    from threading import Thread
-    plot_thread = Thread(target=node.run_plot)
-    plot_thread.start()
-    plt.show()
+    # Run the plot in the same thread as ROS spin
+    node.run_plot()
 
     try:
         rclpy.spin(node)
