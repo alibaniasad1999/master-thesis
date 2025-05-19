3769344e021e4b94a54551b9016f8127155923d9
Ali <alibaniasad1999@yahoo.com>
2025-05-19 12:10:49 +0330

    add sac zs to too algorythms

--- Code/Python/TBP/SAC/ZeroSum/SAC.py
+++ Code/Python/TBP/SAC/ZeroSum/SAC.py
@@ -192,20 +192,16 @@ class ReplayBuffer:
         self.obs_buf = np.zeros(combined_shape(size, obs_dim), dtype=np.float32)
         self.obs2_buf = np.zeros(combined_shape(size, obs_dim), dtype=np.float32)
         self.act_buf = np.zeros(combined_shape(size, act_dim), dtype=np.float32)
-        self.act_1_buf = np.zeros(combined_shape(size, act_dim), dtype=np.float32)
         self.rew_buf = np.zeros(size, dtype=np.float32)
-        self.rew_1_buf = np.zeros(size, dtype=np.float32)
         self.done_buf = np.zeros(size, dtype=np.float32)
         self.ptr, self.size, self.max_size = 0, 0, size
         self.device = device_class
 
-    def store(self, obs, act, act_1, rew, rew_1, next_obs, done):
+    def store(self, obs, act, rew, next_obs, done):
         self.obs_buf[self.ptr] = obs
         self.obs2_buf[self.ptr] = next_obs
         self.act_buf[self.ptr] = act
-        self.act_1_buf[self.ptr] = act_1
         self.rew_buf[self.ptr] = rew
-        self.rew_1_buf[self.ptr] = rew_1
         self.done_buf[self.ptr] = done
         self.ptr = (self.ptr + 1) % self.max_size
         self.size = min(self.size + 1, self.max_size)
@@ -215,14 +211,12 @@ class ReplayBuffer:
         batch = dict(obs=self.obs_buf[idxs],
                      obs2=self.obs2_buf[idxs],
                      act=self.act_buf[idxs],
-                     act_1=self.act_1_buf[idxs],
                      rew=self.rew_buf[idxs],
-                     rew_1=self.rew_1_buf[idxs],
                      done=self.done_buf[idxs])
         return {k: torch.as_tensor(v, dtype=torch.float32, device=self.device) for k, v in batch.items()}
 
 
-class ZS_SAC:
+class SAC:  # TODO: fix device
     def __init__(self, env_fn, actor_critic=MLPActorCritic, ac_kwargs_class=None, seed=0,
                  steps_per_epoch=4000, epochs=100, replay_size=int(1e6), gamma=0.99,
                  polyak=0.995, lr=1e-3, alpha=0.2, batch_size=128, start_steps=10000,
@@ -368,20 +362,12 @@ class ZS_SAC:
         self.ac = actor_critic(self.env.observation_space, self.env.action_space, **self.ac_kwargs)
         self.ac_targ = deepcopy(self.ac)
 
-        # add second player
-        self.ac_1 = actor_critic(self.env.observation_space, self.env.action_space, **self.ac_kwargs)
-        self.ac_1_targ = deepcopy(self.ac_1)
-
         # Freeze target networks with respect to optimizers (only update via polyak averaging)
         for p in self.ac_targ.parameters():
             p.requires_grad = False
 
-        for p in self.ac_1_targ.parameters():
-            p.requires_grad = False
-
         # List of parameters for both Q-networks (save this for convenience)
         self.q_params = itertools.chain(self.ac.q1.parameters(), self.ac.q2.parameters())
-        self.q_params_1 = itertools.chain(self.ac_1.q1.parameters(), self.ac_1.q2.parameters())
 
         # Experience buffer
         self.replay_buffer = ReplayBuffer(obs_dim=self.obs_dim, act_dim=self.act_dim, size=self.replay_size,
@@ -394,12 +380,9 @@ class ZS_SAC:
         # set up optimizers for policy and q-function
         self.pi_optimizer = Adam(self.ac.pi.parameters(), lr=lr)
         self.q_optimizer = Adam(self.q_params, lr=lr)
-        self.pi_optimizer_1 = Adam(self.ac_1.pi.parameters(), lr=lr)
-        self.q_optimizer_1 = Adam(self.q_params_1, lr=lr)
 
         # set up model saving
         self.logger.setup_pytorch_saver(self.ac)
-        self.logger.setup_pytorch_saver(self.ac_1)
 
     # Set up function for computing SAC Q-losses
     def compute_loss_q(self, data):
@@ -430,34 +413,6 @@ class ZS_SAC:
 
         return loss_q, q_info
 
-    def compute_loss_q_1(self, data):
-        o, a, r, o2, d = data['obs'], data['act_1'], data['rew_1'], data['obs2'], data['done']
-
-        q1 = self.ac_1.q1(o, a)
-        q2 = self.ac_1.q2(o, a)
-
-        # Bellman backup for Q functions
-        with torch.no_grad():
-            # Target actions come from *current* policy
-            a2, logp_a2 = self.ac_1.pi(o2)
-
-            # Target Q-values
-            q1_pi_targ = self.ac_1_targ.q1(o2, a2)
-            q2_pi_targ = self.ac_1_targ.q2(o2, a2)
-            q_pi_targ = torch.min(q1_pi_targ, q2_pi_targ)
-            backup = r + self.gamma * (1 - d) * (q_pi_targ - self.alpha * logp_a2)
-
-        # MSE loss against Bellman backup
-        loss_q1 = ((q1 - backup) ** 2).mean()
-        loss_q2 = ((q2 - backup) ** 2).mean()
-        loss_q = loss_q1 + loss_q2
-
-        # Useful info for logging
-        q_info = dict(Q1Vals_1=q1.detach().cpu().numpy(),
-                      Q2Vals_1=q2.detach().cpu().numpy())
-
-        return loss_q, q_info
-
     # Set up function for computing SAC pi loss
     def compute_loss_pi(self, data):
         o = data['obs']
@@ -474,106 +429,47 @@ class ZS_SAC:
 
         return loss_pi, pi_info
 
-    def compute_loss_pi_1(self, data):
-        o = data['obs']
-        pi, logp_pi = self.ac_1.pi(o)
-        q1_pi = self.ac_1.q1(o, pi)
-        q2_pi = self.ac_1.q2(o, pi)
-        q_pi = torch.min(q1_pi, q2_pi)
-
-        # Entropy-regularized policy loss
-        loss_pi = (self.alpha * logp_pi - q_pi).mean()
-
-        # Useful info for logging
-        pi_info = dict(LogPi_1=logp_pi.detach().cpu().numpy())
-
-        return loss_pi, pi_info
-
-    def update(self, data, first_player=0):
+    def update(self, data):
         # First run one gradient descent step for Q1 and Q2
-        if first_player!=0:
-            self.q_optimizer.zero_grad()
-            loss_q, q_info = self.compute_loss_q(data)
-            loss_q.backward()
-            self.q_optimizer.step()
-
-            # Record things
-            self.logger.store(LossQ=loss_q.item(), **q_info)
-        else:
-            print(colorize("First player is not learning 🥱", 'red', bold=True))
-            self.logger.store(LossQ=0.0, **{'Q1Vals': 0.0, 'Q2Vals': 0.0})
-
-        # second player
-        self.q_optimizer_1.zero_grad()
-        loss_q_1, q_info_1 = self.compute_loss_q_1(data)
-        loss_q_1.backward()
-        self.q_optimizer_1.step()
+        self.q_optimizer.zero_grad()
+        loss_q, q_info = self.compute_loss_q(data)
+        loss_q.backward()
+        self.q_optimizer.step()
 
         # Record things
-        self.logger.store(LossQ_1=loss_q_1.item(), **q_info_1)
+        self.logger.store(LossQ=loss_q.item(), **q_info)
 
         # Freeze Q-networks so you don't waste computational effort
         # computing gradients for them during the policy learning step.
         for p in self.q_params:
             p.requires_grad = False
 
-        for p in self.q_params_1:
-            p.requires_grad = False
-
-        if first_player!=0:
-            # Next run one gradient descent step for pi.
-            self.pi_optimizer.zero_grad()
-            loss_pi, pi_info = self.compute_loss_pi(data)
-            loss_pi.backward()
-            self.pi_optimizer.step()
-
-            # Unfreeze Q-networks so you can optimize it at next DDPG step.
-            for p in self.q_params:
-                p.requires_grad = True
-
-            # Record things
-            self.logger.store(LossPi=loss_pi.item(), **pi_info)
-
-            # Finally, update target networks by polyak averaging.
-            with torch.no_grad():
-                for p, p_targ in zip(self.ac.parameters(), self.ac_targ.parameters()):
-                    # NB: We use the in-place operations "mul_", "add_" to update target
-                    # params, as opposed to "mul" and "add", which would make new tensors.
-                    p_targ.data.mul_(self.polyak)
-                    p_targ.data.add_((1 - self.polyak) * p.data)
-        else:
-            self.logger.store(LossPi=0.0, **{'LogPi': 0.0})
-
-        # second player
-        self.pi_optimizer_1.zero_grad()
-        loss_pi_1, pi_info_1 = self.compute_loss_pi_1(data)
-        loss_pi_1.backward()
-        self.pi_optimizer_1.step()
+        # Next run one gradient descent step for pi.
+        self.pi_optimizer.zero_grad()
+        loss_pi, pi_info = self.compute_loss_pi(data)
+        loss_pi.backward()
+        self.pi_optimizer.step()
 
         # Unfreeze Q-networks so you can optimize it at next DDPG step.
-        for p in self.q_params_1:
+        for p in self.q_params:
             p.requires_grad = True
 
         # Record things
-        self.logger.store(LossPi_1=loss_pi_1.item(), **pi_info_1)
+        self.logger.store(LossPi=loss_pi.item(), **pi_info)
+
         # Finally, update target networks by polyak averaging.
         with torch.no_grad():
-            for p, p_targ in zip(self.ac_1.parameters(), self.ac_1_targ.parameters()):
+            for p, p_targ in zip(self.ac.parameters(), self.ac_targ.parameters()):
                 # NB: We use the in-place operations "mul_", "add_" to update target
                 # params, as opposed to "mul" and "add", which would make new tensors.
                 p_targ.data.mul_(self.polyak)
                 p_targ.data.add_((1 - self.polyak) * p.data)
 
-
     def get_action(self, o, deterministic=False):
         return self.ac.act(torch.as_tensor(o, dtype=torch.float32, device=self.device),
                            deterministic)
 
-    def get_action_1(self, o, deterministic=False):
-        return self.ac_1.act(torch.as_tensor(o, dtype=torch.float32, device=self.device),
-                           deterministic)
-
-    def train(self, epochs=300, first_player_epoch_learning=50):
+    def train(self, epochs=1000):
         # Prepare for interaction with environment
         total_steps = self.steps_per_epoch * epochs
         start_time = time.time()
@@ -584,14 +480,13 @@ class ZS_SAC:
             # Until start_steps have elapsed, randomly sample actions
             # from a uniform distribution for better exploration. Afterward,
             # use the learned policy.
-            a = self.get_action(o)
             if t > self.start_steps:
-                a_1 = self.get_action_1(o)
+                a = self.get_action(o)
             else:
-                a_1 = self.env.action_space.sample()
+                a = self.env.action_space.sample()
 
             # Step the env
-            o2, r, d, _, _ = self.env.step(a, a_1)
+            o2, r, d, _, _ = self.env.step(a)
             ep_ret += r
             ep_len += 1
 
@@ -600,8 +495,8 @@ class ZS_SAC:
             # that isn't based on the agent's state)
             d = False if ep_len == self.max_ep_len else d
 
-            # Store experience to replay buffer (self, obs, act, act_1, rew, rew_1, next_obs, done)
-            self.replay_buffer.store(o, a, a_1, r, -r, o2, d)
+            # Store experience to replay buffer
+            self.replay_buffer.store(o, a, r, o2, d)
 
             # Supercritical, easy to overlook a step: make sure to update
             # the most recent observation!
@@ -618,7 +513,7 @@ class ZS_SAC:
                 print(colorize("updating 🥰", 'blue', bold=True))
                 for j in range(self.num_updates):
                     batch = self.replay_buffer.sample_batch(self.batch_size)
-                    self.update(data=batch, first_player=0 if j < first_player_epoch_learning else 1)
+                    self.update(data=batch)
 
             # End of epoch handling
             if (t + 1) % self.steps_per_epoch == 0:
@@ -637,14 +532,9 @@ class ZS_SAC:
                 self.logger.log_tabular('TotalEnvInteracts', t)
                 self.logger.log_tabular('Q1Vals', with_min_and_max=True)
                 self.logger.log_tabular('Q2Vals', with_min_and_max=True)
-                self.logger.log_tabular('Q1Vals_1', with_min_and_max=True)
-                self.logger.log_tabular('Q2Vals_1', with_min_and_max=True)
                 self.logger.log_tabular('LogPi', with_min_and_max=True)
-                self.logger.log_tabular('LogPi_1', with_min_and_max=True)
                 self.logger.log_tabular('LossPi', average_only=True)
-                self.logger.log_tabular('LossPi_1', average_only=True)
                 self.logger.log_tabular('LossQ', average_only=True)
-                self.logger.log_tabular('LossQ_1', average_only=True)
                 self.logger.log_tabular('Time', time.time() - start_time)
                 self.logger.dump_tabular()
 
@@ -715,20 +605,6 @@ class ZS_SAC:
             torch.save(self.ac.q2.state_dict(), filepath + 'q2_cpu.pth')
         print(colorize(f"Model saved successfully! 🥰😎", 'blue', bold=True))
 
-    def save_1(self, filepath='model/'):
-        if not os.path.isdir(filepath):
-            os.mkdir(filepath)
-        # Check the device_ of the model
-        if self.ac_1.pi.device.type == 'cuda':
-            torch.save(self.ac_1.pi.state_dict(), filepath + 'actor_1_cuda.pth')
-            torch.save(self.ac_1.q1.state_dict(), filepath + 'q1_1_cuda.pth')
-            torch.save(self.ac_1.q2.state_dict(), filepath + 'q2_1_cuda.pth')
-        else:
-            torch.save(self.ac_1.pi.state_dict(), filepath + 'actor_1_cpu.pth')
-            torch.save(self.ac_1.q1.state_dict(), filepath + 'q1_1_cpu.pth')
-            torch.save(self.ac_1.q2.state_dict(), filepath + 'q2_1_cpu.pth')
-        print(colorize(f"Model 1 saved successfully! 🥰😎", 'blue', bold=True))
-
     # load actor critic
     def load(self, filepath='model/', load_device=torch.device("cpu"), from_device_to_load='cpu'):
         # check if the model is available
@@ -758,35 +634,6 @@ class ZS_SAC:
             print(colorize(f"Model loaded successfully and device is {load_device}! 🥰😎", 'blue', bold=True))
         else:
             print(colorize("Model not found! 😱🥲", 'red', bold=True))
-
-    def load_1(self, filepath='model/', load_device=torch.device("cpu"), from_device_to_load='cpu'):
-        # check if the model is available
-        if os.path.isfile(filepath + 'actor_1_cpu.pth') or os.path.isfile(filepath + 'actor_1_cuda.pth'):
-            # Check the device_ of the model
-            if from_device_to_load == 'cpu':
-                actor_file = 'actor_1_cpu.pth'
-                q1_file = 'q1_1_cpu.pth'
-                q2_file = 'q2_1_cpu.pth'
-            else:
-                actor_file = 'actor_1_cuda.pth'
-                q1_file = 'q1_1_cuda.pth'
-                q2_file = 'q2_1_cuda.pth'
-
-            if from_device_to_load == 'cpu' and load_device.type == 'cuda':
-                self.ac_1.pi.load_state_dict(torch.load(filepath + actor_file, map_location=torch.device('cuda')))
-                self.ac_1.q1.load_state_dict(torch.load(filepath + q1_file, map_location=torch.device('cuda')))
-                self.ac_1.q2.load_state_dict(torch.load(filepath + q2_file, map_location=torch.device('cuda')))
-            elif from_device_to_load == 'cuda' and load_device.type == 'cpu':
-                self.ac_1.pi.load_state_dict(torch.load(filepath + actor_file, map_location=torch.device('cpu')))
-                self.ac_1.q1.load_state_dict(torch.load(filepath + q1_file, map_location=torch.device('cpu')))
-                self.ac_1.q2.load_state_dict(torch.load(filepath + q2_file, map_location=torch.device('cpu')))
-            else:
-                self.ac_1.pi.load_state_dict(torch.load(filepath + actor_file))
-                self.ac_1.q1.load_state_dict(torch.load(filepath + q1_file))
-                self.ac_1.q2.load_state_dict(torch.load(filepath + q2_file))
-            print(colorize(f"Model 1 loaded successfully and device is {load_device}! 🥰😎", 'blue', bold=True))
-        else:
-            print(colorize("Model 1 not found! 😱🥲", 'red', bold=True))
 
 
 
